2ba7d85f559345201718a85239a46b6396b3cecb
A-Behairi <behairiahmed@gmail.com>
2023-02-13 17:37:57 +0200

    update

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -2,11 +2,12 @@
     <v-card @click="switchRoute" class="card">
         <v-img src="../assets/cardImage.png"></v-img>
         <slot name="audio"></slot>
-        <v-card-title class="card-title">
-            Header Title
+        <v-card-title >
+            <h2 class="card-title">Header Title</h2>
         </v-card-title>
-        <v-card-text class="card-text">
-            {{ cardText }}
+        <v-card-text >
+         <p class="card-text"> {{ cardText }}</p>
+            
         </v-card-text>
     </v-card>
  </template>
@@ -20,11 +21,11 @@ const router = useRouter()
 const cardText:ComputedRef<string> = computed(()=>{
 if (route.path == '/audio' ) {
     return 'Description: sit pellentesque volutpat aliquet ultricies. Risus blandit §§'
-    
 } else {
     return 'Description: sit pellentesque volutpat aliquet ultricies. Risus blandit §§ sit pellentesque volutpat aliquet ultricies. Risus blan'
 }
 })
+
 const switchRoute = ()=>{
     route.path == '/articles' ? router.push('/audio'):router.push('/articles')
 }
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,6 +1,5 @@
 <template>
   <v-main class="main-section d-flex flex-column">
-    <!-- <v-container class="d-flex flex-column align-center"> -->
       <v-row class="d-flex align-start my-3">
         <v-col class="text-center">
           <h1 class="heading">Header Text</h1>
@@ -20,51 +19,16 @@
           </CardComponent>
         </v-col>
       </v-row>
-    <!-- </v-container> -->
   </v-main>
 </template>
 
 <script lang="ts" setup>
 import { defineAsyncComponent } from 'vue';
+
+
 const CardComponent = defineAsyncComponent(()=> import('@/components/CardComponent.vue'))
 
 </script>
-<style scoped>
-.main-section {
-  background-color: #015C89;
-  min-height: 100vh;
-}
-
-.heading{
-
-
-font-family: 'Montserrat';
-font-style: normal;
-font-weight: 700;
-font-size: 98px;
-line-height: 119px;
-text-transform: capitalize;
-color: #CCCCCC;
-
-}
-
-.subheading{
-
-/* Subheading */
-
-font-family: 'Montserrat';
-font-style: normal;
-font-weight: 400;
-font-size: 32px;
-line-height: 150.9%;
-text-transform: capitalize;
-
-color: #FFFFFF;
-}
-
-.play-button{
-  position: relative;
-  padding: 20px;
-  margin: 20px;
-}
+<style lang="scss" scoped>
+@use '@/styles/main.scss'
 </style>
